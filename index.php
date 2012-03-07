30a79b23b28918665a2a1bbbeebe067cce78dda5
Pim van den Berg <pim@nethuis.nl>
2012-03-07 22:00:54 +0100

    move functions to a separate file

--- index.php
+++ index.php
@@ -1,27 +1,8 @@
 <?php
 
+require_once 'inc/functions.php';
+
 $file = 'bonnie.csv';
-
-function parse_bonnie_csv($file) {
-	if (($handle = fopen("$file", "r")) !== FALSE) {
-		# bonnie++ 1.96 column names
-		$col_names = array('vera', 'verb', 'name', 'conc', 'stz', 'sz', 'tta', 'outch', 'outchcpu', 'outblk', 'outblkcpu', 'outrw', 'outrwcpu', 'inch', 'inchcpu', 'inblk', 'inblkcpu', 'seek', 'seekcpu', 'ttb', 'ttc', 'ttd', 'tte', 'ttf', 'sc', 'sccpu', 'sr', 'srcpu', 'sd', 'sdcpu', 'rc', 'rccpu', 'rr', 'rrcpu', 'rd', 'rdcpu', 'latoutch', 'latoutblk', 'latoutrw', 'latinch', 'latinblk', 'latrand', 'latsc', 'latsr', 'latsd', 'latrc', 'latrr', 'latrd');
-
-		while (($line = fgetcsv($handle, 1000, ",")) !== FALSE) {
-			if (count($col_names) != count($line))
-				die('not enough columns');
-
-			foreach (array_combine($col_names, $line) as $k=>$v) {
-				$csv[$k][] = $v;
-			}
-		}
-
-		fclose($handle);
-	}
-
-	return $csv;
-}
-
 $data = parse_bonnie_csv($file);
 
 echo <<<EOT
@@ -39,10 +20,6 @@ EOT;
 
 foreach ($data['name'] as $val) {
 	printf("        data.addColumn('number', '%s')\n", $val);
-}
-
-function addRow($rows, $desc) {
-	return sprintf("data.addRows([['%s', %s]]);\n", $desc, join($rows, ', '));
 }
 
 echo addRow($data['outblk'], 'Seq Block Output');
