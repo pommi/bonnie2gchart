68ac144612b516e7c9e6f71e5f84cb777d8715eb
Pim van den Berg <pim@nethuis.nl>
2012-03-21 13:08:23 +0100

    make frontpage link to all possible graphs

--- index.php
+++ index.php
@@ -6,6 +6,25 @@ include_once 'inc/labels.php';
 $file = 'bonnie.csv';
 $data = parse_bonnie_csv($file);
 
+$types = array(
+	'blockio' => 'Block IO',
+	'metadata' => 'File metadata',
+	'metadata-read' => 'File metadata (read)',
+	'blockio-cpu' => 'Block IO CPU',
+	'metadata-cpu' => 'Seq and Random CPU',
+	'blockio-latency' => 'Block IO Latency',
+	'metadata-latency' => 'File metadata Latency',
+	'metadata-read-latency' => 'File metadata (read) Latency',
+);
+
+if (!isset($_GET['t']) || empty($_GET['t'])) {
+	echo "<html>\n<body>\n<title>Bonnie to Google Chart</title>\n<h1>Bonnie to Google Chart</h1>\n<ul>";
+	foreach ($types as $key => $type) {
+		printf('<li><a href="?t=%s">%s</a></li>', $key, $type);
+	}
+	echo "</ul>\n</body>\n</html>\n";
+}
+
 echo <<<EOT
 <html>
   <head>
@@ -23,46 +42,38 @@ foreach ($data['name'] as $val) {
 	printf("        data.addColumn('number', '%s')\n", $val);
 }
 
+$vtitle = $types[$_GET['t']];
 switch ($_GET['t']) {
 	case 'blockio':
-	default:
 		$title = 'kB/sec (higher is better)';
-		$vtitle = 'Block IO';
 		$types = array('outblk', 'outrw', 'inblk');
 		break;
 	case 'metadata':
 		$title = 'files/sec (higher is better)';
-		$vtitle = 'File metadata';
 		$types = array('sc', 'sd', 'rc', 'rd');
 		break;
 	case 'metadata-read':
 		$title = 'files/sec (higher is better)';
-		$vtitle = 'File metadata (read)';
 		$types = array('sr', 'rr');
 		break;
 	case 'blockio-cpu':
 		$title = 'CPU usage in % (lower is better)';
-		$vtitle = 'Block IO CPU';
 		$types = array('outblkcpu', 'outrwcpu', 'inblkcpu', 'seekcpu');
 		break;
 	case 'metadata-cpu':
 		$title = 'CPU usage in % (lower is better)';
-		$vtitle = 'Seq and Random CPU';
 		$types = array('sccpu', 'srcpu', 'sdcpu', 'rccpu', 'rrcpu', 'rdcpu');
 		break;
 	case 'blockio-latency':
 		$title = 'milliseconds (lower is better)';
-		$vtitle = 'Block IO Latency';
 		$types = array('latoutblk', 'latoutrw', 'latinblk');
 		break;
 	case 'metadata-latency':
 		$title = 'milliseconds (lower is better)';
-		$vtitle = 'File metadata Latency';
 		$types = array('latsc', 'latsd', 'latrc', 'latrd');
 		break;
 	case 'metadata-read-latency':
 		$title = 'milliseconds (lower is better)';
-		$vtitle = 'File metadata (read) Latency';
 		$types = array('latsr', 'latrr');
 		break;
 }
