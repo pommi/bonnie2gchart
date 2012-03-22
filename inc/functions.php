9e21d01bb20e83801d864e7563f384e787e2cfd9
Pim van den Berg <pim@nethuis.nl>
2012-03-22 14:53:28 +0100

    support for bonnie 1.03
    TODO: 1.03 can't be used in combination with 1.96.

--- inc/functions.php
+++ inc/functions.php
@@ -2,12 +2,20 @@
 
 function parse_bonnie_csv($file) {
 	if (($handle = fopen("$file", "r")) !== FALSE) {
+		# bonnie++ 1.03 column names
+		$col_103 = array('vera', 'verb', 'name', 'conc', 'stz', 'sz', 'tta', 'outch', 'outchcpu', 'outblk', 'outblkcpu', 'outrw', 'outrwcpu', 'inch', 'inchcpu', 'inblk', 'inblkcpu', 'seek', 'seekcpu', 'files', 'sc', 'sccpu', 'sr', 'srcpu', 'sd', 'sdcpu', 'rc', 'rccpu', 'rr', 'rrcpu', 'rd', 'rdcpu');
 		# bonnie++ 1.96 column names
-		$col_names = array('vera', 'verb', 'name', 'conc', 'stz', 'sz', 'tta', 'outch', 'outchcpu', 'outblk', 'outblkcpu', 'outrw', 'outrwcpu', 'inch', 'inchcpu', 'inblk', 'inblkcpu', 'seek', 'seekcpu', 'ttb', 'ttc', 'ttd', 'tte', 'ttf', 'sc', 'sccpu', 'sr', 'srcpu', 'sd', 'sdcpu', 'rc', 'rccpu', 'rr', 'rrcpu', 'rd', 'rdcpu', 'latoutch', 'latoutblk', 'latoutrw', 'latinch', 'latinblk', 'latrand', 'latsc', 'latsr', 'latsd', 'latrc', 'latrr', 'latrd');
+		$col_196 = array('vera', 'verb', 'name', 'conc', 'stz', 'sz', 'tta', 'outch', 'outchcpu', 'outblk', 'outblkcpu', 'outrw', 'outrwcpu', 'inch', 'inchcpu', 'inblk', 'inblkcpu', 'seek', 'seekcpu', 'ttb', 'ttc', 'ttd', 'tte', 'ttf', 'sc', 'sccpu', 'sr', 'srcpu', 'sd', 'sdcpu', 'rc', 'rccpu', 'rr', 'rrcpu', 'rd', 'rdcpu', 'latoutch', 'latoutblk', 'latoutrw', 'latinch', 'latinblk', 'latrand', 'latsc', 'latsr', 'latsd', 'latrc', 'latrr', 'latrd');
 
 		while (($line = fgetcsv($handle, 1000, ",")) !== FALSE) {
-			if (count($col_names) != count($line))
+			if (count($col_103) != count($line) && count($col_196) != count($line))
 				die('not enough columns');
+
+			if (count($col_103) == count($line)) {
+				$col_names = $col_103;
+			} elseif (count($col_196) == count($line)) {
+				$col_names = $col_196;
+			}
 
 			foreach (array_combine($col_names, $line) as $k=>$v) {
 
